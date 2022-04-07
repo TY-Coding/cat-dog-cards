46c0e55c0a1f2bae9f26c65914eddbee9c020f59
ouyangyuro <def2446@gmail.com>
2022-04-07 20:41:55 +0800

    (R)4/7 finis login & BackEnd & auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
 import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 //====== below components start ======//
-import Blob from './components/blob';
+import { Blob, Blob1 } from './components/blob';
 import Card from './components/CardDetail';
+import Login from './components/Login';
+import BackEnd from './components/BackEnd';
 //====== below components end ======//
 
 //====== below icon start ======//
 import { MoonIcon, SunIcon } from '@heroicons/react/outline';
 //====== above icon end ======//
 
+//====== createContext start ======//
+import { DataContext } from './utils/context';
+//====== createContext end ======//
+
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [auth, setAuth] = useState(false);
 
   //====== 黑白主題改變時切換設定 start ======//
   const toggleTheme = () => {
@@ -30,29 +37,50 @@ function App() {
   //====== 黑白主題改變時切換設定  ======//
 
   return (
-    <>
-      <div className="w-full h-full min-h-screen px-4 py-16 text-slate-800 dark:text-blue-50 bg-blue-50 dark:bg-slate-800 relative">
-        <div className="w-full max-w-xl p-2 mx-auto relative">
-          <Card />
-          <div className="absolute top-1/3 right-1/4 w-[650px] z-10">
-            <Blob />
+    <DataContext.Provider value={{ auth, setAuth }}>
+      <Router>
+        <>
+          <div className="w-full h-full min-h-screen px-4 py-16 text-slate-800 dark:text-blue-50 bg-blue-50 dark:bg-slate-800 overflow-hidden">
+            <div className="w-full max-w-xl p-2 mx-auto relative">
+              <Switch>
+                <Route path="/back">
+                  <BackEnd />
+                </Route>
+                <Route path="/login">
+                  <Login />
+                </Route>
+                <Route exact path="/">
+                  <Card />
+                </Route>
+              </Switch>
+              <div className="absolute top-1/3 right-1/4 w-[650px] z-10">
+                <Blob />
+              </div>
+              <div className="absolute bottom-1/4 left-1/3 w-[650px] z-10">
+                <Blob />
+              </div>
+              <div className="absolute bottom-[25%] left-[110%] w-[950px] z-10">
+                <Blob1 />
+              </div>
+            </div>
           </div>
-        </div>
-        {/* 黑夜白天按鈕 start */}
-        {isDarkMode ? (
-          <SunIcon
-            onClick={toggleTheme}
-            className="w-6 h-6 absolute top-2.5 right-2.5 cursor-pointer"
-          />
-        ) : (
-          <MoonIcon
-            onClick={toggleTheme}
-            className="w-6 h-6 absolute top-2.5 right-2.5 cursor-pointer"
-          />
-        )}
-        {/* 黑夜白天按鈕 end */}
-      </div>
-    </>
+
+          {/* 黑夜白天按鈕 start */}
+          {isDarkMode ? (
+            <SunIcon
+              onClick={toggleTheme}
+              className="w-6 h-6 absolute top-2.5 right-2.5 cursor-pointer text-blue-50 z-20"
+            />
+          ) : (
+            <MoonIcon
+              onClick={toggleTheme}
+              className="w-6 h-6 absolute top-2.5 right-2.5 cursor-pointer text-slate-800 z-20"
+            />
+          )}
+          {/* 黑夜白天按鈕 end */}
+        </>
+      </Router>
+    </DataContext.Provider>
   );
 }
 
---
+++ client/src/components/Login.js
@@ -0,0 +1,128 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+//====== below 取得Context資料 start ======//
+import { useData } from '../utils/context';
+//====== above 取得Context資料 end ======//
+
+function Login() {
+  const { setAuth } = useData(); // 取得登入狀態
+  const history = useHistory();
+
+  //存欄位值
+  const [fields, setFields] = useState({
+    email: '',
+    password: '',
+  });
+
+  //存錯誤訊息
+  const [fieldErrors, setFieldErrors] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleFieldChange = (e) => {
+    const updatedFields = {
+      ...fields,
+      [e.target.name]: e.target.value,
+    };
+    setFields(updatedFields);
+  };
+
+  //當表單有變動的時候
+  const handleFormChange = (e) => {
+    const errorFields = {
+      ...fieldErrors,
+      [e.target.name]: '',
+    };
+    setFieldErrors(errorFields);
+  };
+
+  //當表單有錯誤(不合檢查的時候)的時候
+  const handleFormInvalid = (e) => {
+    e.preventDefault();
+    const errorFields = {
+      ...fieldErrors,
+      [e.target.name]: e.target.validationMessage,
+    };
+    setFieldErrors(errorFields);
+  };
+
+  //接表單輸入的資料
+  const handleSumbmit = (e) => {
+    e.preventDefault(); //記住你輸入時的值,submit時不會清空
+
+    const formData = new FormData(e.target);
+    let checkEmail = formData.get('email');
+    let checkPassword = formData.get('password');
+    if (checkEmail === '1@1.qw' && checkPassword === '123') {
+      setAuth(true);
+      history.push('/back');
+    } else {
+      setAuth(false);
+      alert('帳號或密碼錯誤');
+    }
+  };
+
+  return (
+    <>
+      <div className="wrapper">
+        <div className="card z-20">
+          <div className="p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <form
+              onChange={handleFormChange}
+              onInvalid={handleFormInvalid}
+              onSubmit={handleSumbmit}
+              className="space-y-6"
+            >
+              <h5 className="text-xl font-medium">Login in</h5>
+              <div>
+                <label className="block mb-2 text-sm font-medium ">
+                  account
+                </label>
+                <input
+                  onChange={handleFieldChange}
+                  type="email"
+                  name="email"
+                  value={fields.email}
+                  className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500"
+                  placeholder="name@company.com"
+                  required
+                />
+                {fieldErrors.email !== '' && (
+                  <small className="text-red-500">{fieldErrors.email}</small>
+                )}
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium">
+                  password
+                </label>
+                <input
+                  onChange={handleFieldChange}
+                  type="password"
+                  name="password"
+                  value={fields.password}
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500"
+                  required
+                />
+                {fieldErrors.password !== '' && (
+                  <small className="text-red-500">{fieldErrors.password}</small>
+                )}
+              </div>
+
+              <button
+                type="submit"
+                className="w-full text-blue-50 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Login to Back-end
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Login;
