31e6731fb463285856f71203a496216a216e34c0
TaylorLiu11 <liuayo1111@gmail.com>
2022-07-09 02:08:52 +0800

    Remove unused module and modify docker volumes

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,9 +2,6 @@ import path from 'path';
 import express, {
   json,
 } from 'express';
-import {
-  renderFile,
-} from 'ejs';
 import 'dotenv/config';
 import cors from 'cors';
 import './utils/mongoose';
@@ -24,19 +21,18 @@ app.use(json());
 app.use(express.static(path.join(__dirname, '/../build')));
 // app.use(express.static(path.join(__dirname, '/../apidoc')));
 // app.set('views', __dirname + '/../apidoc');
-app.engine('html', renderFile);
 
 // app.get('/apidoc', async (_req, res) => {
-  //   res.render('index.html');
-  // });
+//   res.render('index.html');
+// });
   
-  app.use('/api', CardRouter);
-  
-  app.get('/*', function (_req, res) {
-    res.sendFile(
-      path.join(__dirname, "/../build/index.html")
-    );
-  });
+app.use('/api', CardRouter);
+
+app.get('/*', function (_req, res) {
+  res.sendFile(
+    path.join(__dirname, "/../build/index.html")
+  );
+});
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}...`);
